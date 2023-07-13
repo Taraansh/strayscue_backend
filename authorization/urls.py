a908553de6a41cc516d769945067e21413c99b1f
Taraansh <taraanshnandwani123@gmail.com>
2023-07-13 22:56:07 +0530

    Extra code removed

--- authorization/urls.py
+++ authorization/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
 from authorization import views
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('login/', views.login, name='login'),
