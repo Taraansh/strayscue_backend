68aa0af6f37583bdf17cbc98e31f56275a940868
Taraansh <taraanshnandwani123@gmail.com>
2023-07-21 22:43:51 +0530

    Added update routes

--- case_management/urls.py
+++ case_management/urls.py
@@ -13,7 +13,9 @@ urlpatterns = [
     path('addoperational/', views.create_case_operation_details, name='create_case_medical'),
     path('addpostop/', views.create_case_post_operation_details, name='create_case_post_operation_details'),
 
-
-    # path('updatereporter/<int:case_id>/', views.update_case, name='case-update'),
-    # path('deletereporter/<int:case_id>/', views.delete_case, name='case-delete'),
+    path('updatereporter/<int:id>/', views.update_reporter, name='report_update'),
+    path('updateanimal/<int:id>/', views.update_animal, name='update_animal'),
+    path('updatemedical/<int:id>/', views.update_medical, name='update_medical'),
+    path('updateoperational/<int:id>/', views.update_operation, name='update_operation'),
+    path('updatepostoperational/<int:id>/', views.update_post_operation, name='update_post_operation'),
 ]
