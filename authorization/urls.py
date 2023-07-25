5597820484933fabd2dcf5987143eb0b809572a8
Taraansh <taraanshnandwani123@gmail.com>
2023-07-25 19:38:54 +0530

    Added routes for update pwd and profile

--- authorization/urls.py
+++ authorization/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('routes/', views.get_routes),
     path('token/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    path('passchange/<str:email>/', views.password_change, name='change_password'),
+    path('update/<str:email>/', views.update_profile, name="update_profile"),
+    path('getprofile/<str:email>/', views.get_profile, name="get_profile" ),
 ]
