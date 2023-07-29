723e1cafe0369c25f8ade8cb7ddc7a5ba7ae3179
Taraansh <taraanshnandwani123@gmail.com>
2023-07-29 15:09:53 +0530

    Modified admin panel

--- strayscue_backend/urls.py
+++ strayscue_backend/urls.py
@@ -18,6 +18,10 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 
+admin.site.site_header="Strayscue Administration"
+admin.site.site_title="Strayscue Administration"
+admin.site.index_title="Welcome to Administration Panel"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('authorize/', include('authorization.urls')),
