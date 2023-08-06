86e03368a0fc5aeda4a17528cae26b2beadc02be
Taraansh <taraanshnandwani123@gmail.com>
2023-08-06 22:43:13 +0530

    Added new urls

--- case_management/urls.py
+++ case_management/urls.py
@@ -16,4 +16,9 @@ urlpatterns = [
     path('updatepostoperational/<int:id>/', views.update_post_operation, name='update_post_operation'),
 
     path('deleteanimalpicture/<int:id>/', views.delete_animal_picture, name='delete_animal_picture'),
+    path('deletefeedingrecord/<int:id>/', views.delete_feeding_record_image, name='delete_feeding_record_image'),
+    path('deletetreatmentrecord/<int:id>/', views.delete_treatment_record_image, name='delete_treatment_record_image'),
+    path('deleteorganimage/<int:id>/', views.delete_organ_image, name='delete_organ_image'),
+    path('deletepostoperationpicture/<int:id>/', views.delete_pop_pictures, name='delete_pop_pictures'),
+    path('deletereleasepicture/<int:id>/', views.delete_release_pictures, name='delete_release_pictures'),
 ]
