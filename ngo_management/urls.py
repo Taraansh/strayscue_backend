7c699d0ae419fd24b0b5cfc741fd82cacbb06194
Taraansh <taraanshnandwani123@gmail.com>
2023-10-29 10:12:21 +0530

    Added new path

--- ngo_management/urls.py
+++ ngo_management/urls.py
@@ -3,10 +3,13 @@ from ngo_management import views
 
 
 urlpatterns = [
-    path('all/<str:email>/', views.get_all_ngos_using_email, name='get_all_ngos_using_email'),
+    path('all/<str:email>/', views.get_all_ngos_using_email,
+         name='get_all_ngos_using_email'),
     path('addngo/', views.add_ngo, name='add_ngo'),
+    path('getngo/<int:id>/', views.get_ngo, name='get_ngo'),
     path('delete/<int:id>/', views.delete_ngo, name='delete_ngo'),
     path('update/<int:id>/', views.update_ngo, name='update_ngo'),
-    path('allusers/<str:email>/', views.get_all_ngo_linked_user, name='get_all_ngo_linked_user'),
+    path('allusers/<str:email>/', views.get_all_ngo_linked_user,
+         name='get_all_ngo_linked_user'),
     path('allngos/', views.get_all_ngos, name="get_all_ngos"),
 ]
